03a8ef560434e80ea2d8dfc9e1005652b6b1cd90
Kabzeel <ian.heltonn@gmail.com>
2022-02-12 10:01:32 -0500

    css

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
 /*-------------------------------- Constants --------------------------------*/
 
 
-
 /*-------------------------------- Variables --------------------------------*/
-let youWin, 
 
 
 
 /*-------------------------------- Constants Element References--------------------------------*/
-const machine
-
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 
 
-
 /*-------------------------------- Functions --------------------------------*/
+
+
+
+// i need to be able to click the spin button and make the haracters in each cell change by the colums they are in.
+
+//
