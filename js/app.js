dd74bcf3c736fa279bf604545229610c09ce765a
Kabzeel <ian.heltonn@gmail.com>
2022-02-12 11:08:18 -0500

    css

--- js/app.js
+++ js/app.js
@@ -2,19 +2,21 @@
 
 
 /*-------------------------------- Variables --------------------------------*/
-
+let gameWon, spin
 
 
 /*-------------------------------- Constants Element References--------------------------------*/
+const spinButton = document.querySelectorAll('.cell')
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+spinButton.addEvenListener('click', spinClicked)
+console.log(spi)
 
 
 /*-------------------------------- Functions --------------------------------*/
 
 
 
-// i need to be able to click the spin button and make the haracters in each cell change by the colums they are in.
+// i need to be able to click the spin button and make the characters in each cell change by the colums they are in.
 
 //
