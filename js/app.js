e0bbcdfcadab916dde895c4e835413e5359e4592
Kabzeel <ian.heltonn@gmail.com>
2022-02-12 21:48:27 -0500

    random number

--- js/app.js
+++ js/app.js
@@ -5,18 +5,23 @@
 
 
 /*-------------------------------- Constants Element References--------------------------------*/
-
+button = document.querySelector('.spin')
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+button.addEventListener('click', function(event){
+  return getRandomNumber
+})
 
 
 /*-------------------------------- Functions --------------------------------*/
 
-function getRandomNumber() {
-  return Math.floor(Math.random() * 2) + 1;
-}
+eo
 
+emo = function getRandomNumber() {
+  
+  return Math.floor(Math.random() * emoji) + 1;
+}
+getRandomNumber();
 function getElement(id) {
   return document.getElementById(id);
 }
@@ -25,5 +30,37 @@ function spin() {
   const item1 = getElement('item1');
   const item2 = getElement('item2');
   const item3 = getElement('item3');
-
+  getRandomNumber();
+  const num1 = getRandomNumber();
+  const num2 = getRandomNumber();
+  const num3 = getRandomNumber();
+console.log(num1)
   
+item1.innerHTML = `${num1}`;
+  item2.innerHTML = `${num2}`;
+  item3.innerHTML = `${num3}`;
+
+  // if (num1 === num2 && num1 === num3) {
+  //     playBoing();
+  //     showMessage();
+  // } else {
+  //     hideMessage();
+  // }
+}
+spin();
+
+// function playBoing() {
+//   const boing = document.getElementById("boing");
+//   boing.play();
+// }
+
+function showMessage() {
+  const msg = document.getElementById("message");
+  msg.style.display = "block";
+  msg.classList.add('animated', 'pulse')
+}
+
+// function hideMessage() {
+//   const msg = document.getElementById("message");
+//   msg.style.display = "none";
+// }
