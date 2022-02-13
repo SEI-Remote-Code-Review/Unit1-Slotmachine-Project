2ee280e9b022c3807b7db31540e28cff11c65bc1
Kabzeel <ian.heltonn@gmail.com>
2022-02-12 19:44:09 -0500

    spi

--- js/app.js
+++ js/app.js
@@ -17,3 +17,13 @@ function getRandomNumber() {
   return Math.floor(Math.random() * 2) + 1;
 }
 
+function getElement(id) {
+  return document.getElementById(id);
+}
+
+function spin() {
+  const item1 = getElement('item1');
+  const item2 = getElement('item2');
+  const item3 = getElement('item3');
+
+  
