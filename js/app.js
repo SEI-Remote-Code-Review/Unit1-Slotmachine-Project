c23115deb4e4e4ffeeaa016eb7e7b7ce0767ab0b
Kabzeel <ian.heltonn@gmail.com>
2022-02-16 18:13:18 -0500

    css

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 /*-------------------------------- Constants --------------------------------*/
 const characters = ['❤️', '💰', '⚽', '💎', '🤑']
+const winningMessage =  `You have won`
 
-/*-------------------------------- Variables --------------------------------*/
-let char
+/*-------------------------------- variables--------------------------------*/
+ let char
 
-/*-------------------------------- Constants Element References--------------------------------*/
-button = document.querySelector('.spin')
+/*-------------------------------- Cached Element References--------------------------------*/
+
+var button = document.querySelector('.spin')
+var  win = document.getElementById('win')
+
 
 /*----------------------------- Event Listeners -----------------------------*/
 button.addEventListener('click', function(event){
@@ -28,6 +32,7 @@ function spin() {
   const item1 = getElement('item1');
   const item2 = getElement('item2');
   const item3 = getElement('item3');
+
   getRandomNumber();
   const num1 = getRandomNumber();
   const num2 = getRandomNumber();
@@ -38,28 +43,10 @@ item1.innerHTML = `${num1}`;
   item2.innerHTML = `${num1}`;
   item3.innerHTML = `${num3}`;
 
-   if (num1 === num3) {
-  //     playBoing();
-  //     showMessage();
-  // } else {
-  //     hideMessage();
-  // }
-  }
-}
-spin();
-
-// function playBoing() {
-//   const boing = document.getElementById("boing");
-//   boing.play();
-// }
-
-function showMessage() {
-  const msg = document.getElementById("message");
-  msg.style.display = "block";
-  msg.classList.add('animated', 'pulse')
+   if (num1 === num3){
+    win.innerHTML = winningMessage    
+} else {
+  win.innerHTML = ''
 }
 
-// function hideMessage() {
-//   const msg = document.getElementById("message");
-//   msg.style.display = "none";
-// }
+}
